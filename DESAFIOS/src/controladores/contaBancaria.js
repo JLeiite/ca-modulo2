4c5ee3cfda4d30a653289471e9d01ecffe977151
JLeiite <juliana.leiite@icloud.com>
2023-10-13 17:05:48 -0300

    rotas criadas

--- DESAFIOS/src/controladores/contaBancaria.js
+++ DESAFIOS/src/controladores/contaBancaria.js
@@ -1,4 +1,5 @@
-let { contas, saques, depositos, transferencias, idConta} = require('../bancodedados');
+let { contas, saques, depositos, transferencias, idConta } = require('../bancodedados');
+const dataHoraAtualString = new Date().toLocaleString();
 
 const listarContas = (req, res) => {
     return res.json(contas);
@@ -88,7 +89,6 @@ const efetuarDeposito = (req, res) => {
     }
     
     buscaConta.saldo = buscaConta.saldo + Number(valor);
-    const dataHoraAtualString = new Date().toLocaleString();
     
     const novoDeposito = {
         data: dataHoraAtualString,
@@ -100,15 +100,121 @@ const efetuarDeposito = (req, res) => {
     return res.status(201).json(novoDeposito);
 }
 
-const efetuarSaque = (req, res) => {}
+const efetuarSaque = (req, res) => {
+    const { numero_conta, valor, senha } = req.body;
 
-/*
-const efetuarTransferencia = (req, res) => {}
+    if(!numero_conta || !valor){
+        return res.send({ mensagem: "O número da conta e o valor são obrigatórios!" });
+    } else if(!senha){
+        return res.send({ mensagem: "Senha não informada!" })
+    }
+    else if(valor <= 0){
+        return res.send({ mensagem: "Valor informado é inválido." });
+    }
 
-const efetuarConsultaSaldo = (req, res) => {}
+    let buscaConta = contas.find((contas) => {
+        return contas.numero === Number(numero_conta);
+    });
 
-const listarTransacoes = (req, res) => {}
-*/
+    if(!buscaConta) {
+        return res.status(404).json({ mensagem: "Conta bancária não encontada!" });
+    } else if(buscaConta.usuario.senha !== senha){
+        return res.send({ mensagem: "Senha incorreta"});
+    }
+    
+    if((buscaConta.saldo - Number(valor)) < 0){
+        return res.status(404).json({ mensagem: "Saldo insuficiente!" });
+    }
+    buscaConta.saldo = buscaConta.saldo - Number(valor);
+    
+    const novoSaque = {
+        data: dataHoraAtualString,
+        numero_conta,
+        valor
+    }
+
+    saques.push(novoSaque);
+    return res.status(201).json(novoSaque);
+}
+
+const efetuarTransferencia = (req, res) => {
+    const { numero_conta_origem, numero_conta_destino, valor, senha } = req.body;
+
+    if(!numero_conta_origem || !numero_conta_destino || !valor || !senha){
+        return res.send({ mensagem: "Preencha todos os campos obrigatorios!" });
+    }
+
+    let buscaContaOrigem = contas.find((contas) => {
+        return contas.numero === Number(numero_conta_origem);
+    });
+    if(!buscaContaOrigem) {
+        return res.status(404).json({ mensagem: "Conta bancária não encontada!" });
+    } else if(buscaContaOrigem.usuario.senha !== senha){
+        return res.send({ mensagem: "Senha incorreta"});
+    }
+
+    let buscaContaDestino = contas.find((contas) => {
+        return contas.numero === Number(numero_conta_destino);
+    });
+
+    if(!buscaContaDestino) {
+        return res.status(404).json({ mensagem: "Conta bancária não encontada!" });
+    }
+    
+    if((buscaContaOrigem.saldo - Number(valor)) < 0){
+        return res.status(404).json({ mensagem: "Saldo insuficiente!" });
+    }
+    buscaContaOrigem.saldo = buscaContaOrigem.saldo - Number(valor);
+    buscaContaDestino.saldo = buscaContaDestino.saldo + Number(valor);
+
+    const novaTransferencia = {
+        data: dataHoraAtualString,
+        numero_conta_origem,
+        numero_conta_destino,
+        valor
+    }
+
+    transferencias.push(novaTransferencia);
+    return res.status(201).json(novaTransferencia);
+}
+
+const efetuarConsultaSaldo = (req, res) => {
+    const {numero_conta, senha} = req.query;
+
+    let buscaConta = contas.find((contas) => {
+        return contas.numero === Number(numero_conta_origem);
+    });
+    if(!buscaConta) {
+        return res.status(404).json({ mensagem: "Conta bancária não encontada!" });
+    } else if(buscaConta.numero !== numero_conta){
+        return res.send({ mensagem: "Conta bancária incorreta"});
+    } else if(buscaConta.usuario.senha !== senha){
+        return res.send({ mensagem: "Senha incorreta"});
+    }
+
+    return res.json("Saldo: " + buscaConta.saldo);
+}
+
+const listarTransacoes = (req, res) => {
+    const {numero_conta, senha} = req.query;
+
+    let buscaConta = contas.find((contas) => {
+        return contas.numero === Number(numero_conta_origem);
+    });
+    if(!buscaConta) {
+        return res.status(404).json({ mensagem: "Conta bancária não encontada!" });
+    } else if(buscaConta.numero !== numero_conta){
+        return res.send({ mensagem: "Conta bancária incorreta"});
+    } else if(buscaConta.usuario.senha !== senha){
+        return res.send({ mensagem: "Senha incorreta"});
+    }
+
+    return res.json(
+        "depósitos: " + [buscaConta.transferencias] +
+        "saques: " + [buscaConta.saques] +
+        "transferenciasEnviadas: " + [buscaConta.transferencias]
+    );
+}
 
 module.exports = {
     listarContas,
@@ -117,7 +223,7 @@ module.exports = {
     excluirConta,
     efetuarDeposito,
     efetuarSaque,
-    // efetuarTransferencia,
-    // efetuarConsultaSaldo,
-    // listarTransacoes
+    efetuarTransferencia,
+    efetuarConsultaSaldo,
+    listarTransacoes
 }
--- DESAFIOS/src/rotas.js
+++ DESAFIOS/src/rotas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { listarContas, criarConta, atualizarConta, excluirConta, efetuarDeposito } = require('./controladores/contaBancaria');
+const { listarContas, criarConta, atualizarConta, excluirConta, efetuarDeposito, efetuarSaque, efetuarTransferencia, efetuarConsultaSaldo, listarTransacoes } = require('./controladores/contaBancaria');
 const { validarSenha, validarPreenchimento, validarCPFEmail } = require('./intermediarios');
 
 const rotas = express();
@@ -10,9 +10,9 @@ rotas.post('/contas', validarCPFEmail, criarConta); // validarPreenchimento
 rotas.put('/contas/:numero/usuario', atualizarConta); // validarPreenchimento
 rotas.delete('/contas/:numero', excluirConta);
 rotas.post('/transacoes/depositar', efetuarDeposito);
-// rotas.post('/transacoes/sacar', conta.efetuarSaque);
-// rotas.post('/transacoes/transferir', conta.efetuarTransferencia);
-// rotas.get('/contas/saldo?numero_conta=123&senha=123', conta.efetuarConsultaSaldo);
-// rotas.get('/contas/extrato?numero_conta=123&senha=123', conta.listarTransacoes)
+rotas.post('/transacoes/sacar', efetuarSaque);
+rotas.post('/transacoes/transferir', efetuarTransferencia);
+rotas.get('/contas/saldo?numero_conta=123&senha=123', efetuarConsultaSaldo);
+rotas.get('/contas/extrato?numero_conta=123&senha=123', listarTransacoes)
 
 module.exports = rotas;
